63e8bf938efd62d43dd4805e6008f6ea39d963f9
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2017-03-03 14:06:30 +0100

    Make SDL detection generic to be able to use it in other projects

---
+++ sdl.py
@@ -0,0 +1,48 @@
+"""
+Quick n dirty sdl detection
+"""
+
+import os, glob, types
+from waflib.Configure import conf
+
+
+def options(opt):
+	opt.add_option('--sdl', type='string', help='path to sdl', dest='sdl')
+
+
+@conf
+def check_sdl(conf):
+	if conf.options.sdl:
+		includes_check = [conf.options.sdl + '/include']
+		libs_check = [conf.options.sdl + '/lib']
+	else:
+		includes_check = ['/usr/local/include', '/usr/include']
+		libs_check = ['/usr/local/lib', '/usr/lib', '/usr/lib/x86_64-linux-gnu/']
+		if 'RESIBOTS_DIR' in os.environ:
+			includes_check = [os.environ['RESIBOTS_DIR'] + '/include'] + includes_check
+			libs_check = [os.environ['RESIBOTS_DIR'] + '/lib'] + libs_check
+
+	try:
+		conf.start_msg('Checking for SDL C++ includes')
+		res = conf.find_file('SDL/SDL.h', includes_check)
+		conf.end_msg('ok')
+	except:
+		conf.end_msg('Not found', 'RED')
+		return 1
+	conf.start_msg('Checking for SDL C++ libs')
+	found = False
+	for lib in ['libSDL.so']: #, 'libSDL_image.so']:
+		try:
+			found = found or conf.find_file(lib, libs_check)
+		except:
+			continue
+	if not found:
+		conf.end_msg('Not found', 'RED')
+		return 1
+	else:
+		conf.end_msg('ok')
+		conf.env.INCLUDES_SDL = includes_check
+		conf.env.LIBPATH_SDL = libs_check
+		conf.env.DEFINES_SDL = ['USE_SDL']
+		conf.env.LIB_SDL = ['SDL']#, 'SDL_image']
+	return 1
--- wscript
+++ wscript
@@ -7,31 +7,23 @@ srcdir = '.'
 blddir = 'build'
 
 import copy
-import os, sys, commands
+import os, sys
 import glob
-
-def detect_sdl(conf):
-    conf.start_msg('Checking for SDL')
-    ret = conf.find_program('sdl-config')
-    if not ret:
-        conf.end_msg('ERROR')
-        return 0
-    res = commands.getoutput('sdl-config --cflags --libs')
-    conf.parse_flags(res, uselib_store='SDL')
-    conf.end_msg('ok')
-    return 1
 
 
 def options(opt):
     opt.load('compiler_cxx boost waf_unit_test')
     opt.load('compiler_c')
+    opt.load('sdl')
 
 def configure(conf):
     print("configuring b-optimize")
     conf.load('compiler_cxx boost waf_unit_test')
     conf.load('compiler_c')
-    detect_sdl(conf)
-    common_flags = "-Wall -DUSE_SDL "
+    conf.load('sdl')
+    conf.check_sdl()
+
+    common_flags = "-Wall"
 
     cxxflags = conf.env['CXXFLAGS']
     #    conf.check_boost(lib='',
